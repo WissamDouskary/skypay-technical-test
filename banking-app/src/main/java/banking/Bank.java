f75ded45202014c9e69ee079fb4d6531bf2269ec
WissamDouskary <douskary.wissam@gmail.com>
2025-12-14 16:03:37 +0100

    update tests in main

--- banking-app/src/main/java/banking/Bank.java
+++ banking-app/src/main/java/banking/Bank.java
@@ -6,7 +6,8 @@ public class Bank {
     public static void main(String[] args) {
         AccountServiceImpl ac = new AccountServiceImpl();
 
-        ac.deposit(600);
+        ac.deposit(1000);
+        ac.deposit(2000);
         ac.withdraw(500);
         ac.printStatement();
     }
